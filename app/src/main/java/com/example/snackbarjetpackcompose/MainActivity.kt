1d601fd82917ab6336187051c8c4a99d9c43ee0f
Joel Crawford <joelcrawford30@gmail.com>
2023-12-27 19:11:53 +0300

    Show and hide snackbar the simpleway

--- app/src/main/java/com/example/snackbarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/snackbarjetpackcompose/MainActivity.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -19,46 +19,73 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SnackBarJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val isShowing = remember { mutableStateOf(false) }
+
+            Column() {
+                Button(onClick = {
+                    isShowing.value = true
+                }) {
+                    Text(text = "Show Snackbar")
                 }
+
+                SimpleSnackBarDemo(
+                    isShowing = isShowing.value,
+                    onHideSnackBar = {
+                        isShowing.value = false
+                    }
+                )
+
             }
+
+
+//            SnackBarJetpackComposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
         }
     }
 }
 
 
 @Composable
-fun simpleSnackBarDemo(
+fun SimpleSnackBarDemo(
+    isShowing: Boolean,
     onHideSnackBar: () -> Unit
 ) {
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val snackBar = createRef()
-        Snackbar(
-            modifier = Modifier.constrainAs(snackBar){
-                bottom.linkTo(parent.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            },
-            action = {
-                Text(
-                    text = "Hide",
-                    modifier = Modifier.clickable(
-                        onClick = onHideSnackBar
-                    ),
-                    style = MaterialTheme.typography.bodyLarge
-                )
+    if (isShowing) {
+
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val snackBar = createRef()
+            Snackbar(
+                modifier = Modifier.constrainAs(snackBar) {
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                },
+                action = {
+                    Text(
+                        text = "Hide",
+                        modifier = Modifier.clickable(
+                            onClick = onHideSnackBar
+                        ),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            ) {
+                Text(text = "Hey Look a snack bar")
             }
-        ) {
-            Text(text = "Hey Look a snack bar")
+
         }
 
+    } else {
+
     }
+
 }
 
 @Composable
